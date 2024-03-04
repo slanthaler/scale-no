3fd0b5e7c1a5e0fae12589e8c3c1c6fdb4dfcb9f
Samuel Lanthaler <samuel.lanthaler@sam.math.ethz.ch>
2024-03-04 21:54:18 +0100

    Adding semilinear fno version

--- symmetry_no/fno2d.py
+++ symmetry_no/fno2d.py
@@ -8,6 +8,7 @@ as discussed in Section 5.2 in the [paper](https://arxiv.org/pdf/2010.08895.pdf)
 
 """
 
+import sys
 import torch.nn.functional as F
 from timeit import default_timer
 from symmetry_no.utilities3 import *
@@ -156,3 +157,176 @@ class FNO2d(nn.Module):
         gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
         gridy = gridy.reshape(1, 1, 1, size_y).repeat([batchsize, 1, size_x, 1])
         return torch.cat((gridx, gridy), dim=1).to(device)
+
+
+
+
+
+
+
+
+
+
+
+    
+
+class FNO2dBasic(nn.Module):
+    def __init__(self, modes1, modes2,  width, depth=4, in_channel=1, out_channel=1):
+        super(FNO2dBasic, self).__init__()
+
+        """
+        The overall network. It contains 4 layers of the Fourier layer.
+        1. Lift the input to the desire channel dimension by self.fc0 .
+        2. 4 layers of the integral operators u' = (W + K)(u).
+            W defined by self.w; K defined by self.conv .
+        3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+        
+        input: the solution of the coefficient function, boundary conditions and locations (a(x, y), x, y)
+        input shape: (batchsize, x=s, y=s, c=1)
+        output: the solution 
+        output shape: (batchsize, x=s, y=s, c=1)
+        """
+
+        self.modes1 = modes1
+        self.modes2 = modes2
+        self.width = width
+        self.depth = depth
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+
+        self.p = MLP(self.in_channel+2, self.width, self.width)
+        #self.p = nn.Linear(self.in_channel, self.width) # input channel is 7: (a(x, y), BC_left, BC_bottom, BC_right, BC_top, x, y)
+
+        self.conv = []
+        self.mlp = []
+        self.w = []
+        for _ in range(depth):
+            self.conv.append(SpectralConv2d(self.width, self.width, self.modes1, self.modes2))
+            self.mlp.append(MLP(self.width, self.width, self.width))
+            self.w.append(nn.Conv2d(self.width, self.width, 1))
+        #
+        self.conv = nn.ModuleList(self.conv)
+        self.mlp = nn.ModuleList(self.mlp)
+        self.w = nn.ModuleList(self.w)
+        #
+        self.q = MLP(self.width, self.out_channel, self.width * 4) # output channel is 1: u(x, y)
+
+    def forward(self, x):
+        grid = self.get_grid(x.shape, x.device)
+        x = torch.cat((x, grid), dim=1) # 1 is the channel dimension
+        x = self.p(x)
+
+        for i in range(self.depth):
+            x1 = self.conv[i](x)
+            x1 = self.mlp[i](x1)
+            x2 = self.w[i](x)
+            x = x1 + x2
+            x = F.gelu(x)
+
+        x = self.q(x)
+        return x
+    
+    def get_grid(self, shape, device):
+        batchsize, size_x, size_y = shape[0], shape[2], shape[3]
+        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
+        gridx = gridx.reshape(1, 1, size_x, 1).repeat([batchsize, 1, 1, size_y])
+        gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
+        gridy = gridy.reshape(1, 1, 1, size_y).repeat([batchsize, 1, size_x, 1])
+        return torch.cat((gridx, gridy), dim=1).to(device)
+
+    
+
+    
+class FNO2dSemiLinear(nn.Module):
+    '''
+    Implements a FNO2d to approximate operators Phi of a semi-linear form
+    
+    Phi(a,c1*g1+c2*g2) = c1 * Phi(a,g1) + c2 * Phi(a,g2),
+
+    where g is a one-dimensional input defined over an interval, e.g. representing the boundary condition.
+
+    The structure is as follows. 
+
+    1. We project g onto a fixed basis phi_k,
+    
+    g(x) = \sum_{k=1}^K g_k phi_k(x),
+
+    e.g. phi_k = Fourier basis --> g_k = Fourier coefficients
+
+    2. We have a standard FNO2d with K output channels,
+
+    FNO(a) = [FNO(a)_1, ..., FNO(a)_K]
+
+    3. We combine them in a linear way
+
+    output = \sum_{k=1}^K g_k * FNO(a)_k
+
+    -------------------------------
+    Example: Darcy flow with BC, and domain = the square [0,1]^2
+    
+    -div(a * grad(u)) = 0
+    u|_{boundary} = g
+
+    This is non-linear in a, but linear in g.
+    '''
+
+    def __init__(self, modes1, modes2,  width, depth=4,
+                 in_channel=1, out_channel=1, lin_modes=16):
+        super(FNO2dSemiLinear, self).__init__()
+
+        """
+        input: coefficient function, boundary conditions (a, BC1)
+        input shape: (batchsize, x=s, y=s, c=1), (batchsize, x=s, c=1)
+        output: the solution 
+        output shape: (batchsize, x=s, y=s, c=1)
+        """
+        self.modes1 = modes1
+        self.modes2 = modes2
+        self.width = width
+        self.depth = depth
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+        self.lin_modes = lin_modes
+
+        # non-linear part of the architecture
+        # output shape: (batchsize, x=s, y=s, c=fno_out_channel)
+        if out_channel!=1:
+            raise NotImplementedError(f'{out_channel=} not yet supported. Only out_channel==1 is allowed right now!')
+        # explanation:
+        # 1. factor 2 because Fourier coefficients complex --> real transformation
+        # 2. factor 4 because we concatenate 4 boundary conditions into a (4, x=s) tensor
+        fno_out_channel = out_channel * lin_modes * 2 * 4 # explanation below
+        self.fno = FNO2dBasic(modes1, modes2, width, depth,in_channel, fno_out_channel)
+
+        # linear part of the architecture
+        
+        
+        
+    def forward(self, x):
+        ''' 
+        To be consistent with other models, we keep the structure
+of the inputs. This means that x has shape (batchsize, channels, x=s, y=s).
+
+        '''
+        #
+        batchsize, gridsize = x.shape[0], x.shape[-1]
+        nonlinear = self.fno(x[:,0,:,:].unsqueeze(1))
+
+        # extract BC
+        BC_left   = x[:,1, 0, :].unsqueeze(1) # shape: (B, 1, s)
+        BC_bottom = x[:,2, :, 0].unsqueeze(1)
+        BC_right  = x[:,3,-1, :].unsqueeze(1)
+        BC_top    = x[:,4, :,-1].unsqueeze(1)
+        # combine all BC's
+        BC = torch.cat((BC_left, BC_bottom, BC_right, BC_top), dim=1) # shape: (B, 4, s)
+        #
+        BC_ft = torch.fft.rfft(BC, dim=-1) # shape: (B, 4, s//2+1)
+        BC_ft = BC_ft[:,:,:self.lin_modes] # retain only specified number of modes
+        BC_ft = torch.view_as_real(BC_ft)  # get real-valued array
+        # combine 4 BC channels, with 2*lin_modes 
+        BC_ft = BC_ft.reshape(batchsize, -1)  # shape: (B, 8*lin_modes, 1, 1)
+
+        # combine into semi-linear output
+        output = torch.einsum('bjxy,bc->bxy',nonlinear,BC_ft)
+
+        return output
