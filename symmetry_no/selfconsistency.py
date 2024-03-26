fixing erroneous normalization flag in fno2d. creating two different training scripts, train.py and train_scdiff.py which use conventional selfconsistency and differential selfconsistency, respectively. currently, the differential version is not working, or at least not producing very good results. stick to ordinary for now...

symmetry_no/selfconsistency.py
@@ -77,7 +77,7 @@ def LossSelfconsistencyDiff(model,x,loss_fn):
     G = model(x)                  # model prediction
     #
     vG = FD.rDr(G,mid_pt)         # radial derivative of model output
-    coeff = x[:,0,:,:]            # coefficient field (get rid of BC)
+    coeff = x[:,0,:,:].clone()    # coefficient field (get rid of BC)
     vcoeff = FD.rDr(coeff,mid_pt) # radial derivative of coefficient
 
     # extract BC
@@ -88,8 +88,8 @@ def LossSelfconsistencyDiff(model,x,loss_fn):
     # compute directional derivative <dG/dx,vx> at input x
     pred, dGdx_vx = ft.jvp(model, (x,), (vx,))
 
-    # sanity check
-    assert torch.allclose(pred, G), f'Problem in differential self-consistency loss! Difference {(pred-G).abs().max().item()} should be 0.0'
+    # sanity check (FAILS!)
+    assert torch.allclose(pred, G, atol=1e-5), f'Problem in differential self-consistency loss! Difference {(pred-G).abs().max().item()} should be 0.0'
     
     return loss_fn(vG,dGdx_vx)
 